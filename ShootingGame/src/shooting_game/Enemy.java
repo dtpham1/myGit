b10319a92926c7002ca591241364e4824b0a2ff0
dtpham1 <dtpham1@my.okcu.edu>
2015-04-02 18:25:31 -0500

    This is the first upload for our shooting game for Lab10

---
+++ ShootingGame/src/shooting_game/Enemy.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package shooting_game;
+
+/**
+ *
+ * @author forainy
+ */
+public class Enemy {
+
+    /**
+     * @param args the command line arguments
+     */
+ 
+       //field
+        int x;
+        int y;
+        
+        
+        //method
+        
+        public void move (){
+            x = x-3;
+        }//move
+        
+        
+        //constructor
+        Enemy(int x, int y){
+        
+        this.x = this.x+x;
+        this.y = this.y+y;
+    }//Enemy con
+        
+    }
+    
+
