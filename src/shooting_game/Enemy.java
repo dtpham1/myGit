1bcd0eac2d2f02a1a7fc5b65c1b7037307d10150
dtpham1 <dtpham1@my.okcu.edu>
2015-04-23 19:41:15 -0500

    Pizza-mon, spawn rate. Images moved to img folder.

---
+++ src/shooting_game/Enemy.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gpa;
+
+/**
+ *
+ * @author forainy
+ */
+public class Enemy {
+
+	/**
+	 * @param args the command line arguments
+	 */
+
+	//field
+	int x;
+	int y;
+	int enemyType;
+	static final String[] ENEMY_TYPES = {"img/fb-mon.gif", "img/netflix-mon.gif", "img/pizza-mon.gif"};
+
+
+	//method
+
+	public void move (){
+		x = x-3;
+	}//move
+
+	public void move (int speed){
+		x = x-speed;
+	}
+
+
+	//constructor
+	Enemy(int x, int y, int enemyType){
+
+		this.x = this.x+x;
+		this.y = this.y+y;
+		this.enemyType = enemyType % ENEMY_TYPES.length;
+	}//Enemy con
+
+}
+
+
